bfa3e04a9ad31a04871cad8b10fd974ec7b35b96
harisali241 <harisali241@gmail.com>
2026-02-06 02:12:13 +0500

    u

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// const http = require("http");
 
-// const PORT = process.env.PORT || 3000;
-// http.createServer((req, res) => {
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end("OK");
-// }).listen(PORT, "0.0.0.0", () => {
-//   console.log(`[Bot] Health server listening on ${PORT}`);
-// });
 
 const fs = require("fs");
 const path = require("path");
